c417c5968ed40f3ab91b64eed0963d30f1617203
noman-swe <noman.swe130@gmail.com>
2022-02-16 02:27:47 +0600

    26-7 Update deposit and balance and handle string add

--- js/banking.js
+++ js/banking.js
@@ -2,11 +2,25 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
     // get the amount diposited
     const depositInput = document.getElementById('deposit-input');
-    const depositAmount = depositInput.value;
-    console.log(depositAmount);
+    const newDepositText = depositInput.value;
+    const newDepositAmount = parseFloat(newDepositText);
+    //console.log(depositAmount);
 
+    // update deposit total
     const depositTotal = document.getElementById('deposit-total');
-    depositTotal.innerText = depositAmount; 
+    const previousDepositAmountText = depositTotal.innerText;
+    const previousDepositAmount = parseFloat(previousDepositAmountText);
+    const newDepositTotal = previousDepositAmount + newDepositAmount;
+    depositTotal.innerText = newDepositTotal; 
+
+
+    // update account balance
+    const balanceTotal =  document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
+
 
     //clear the deposit input
     depositInput.value = '';
