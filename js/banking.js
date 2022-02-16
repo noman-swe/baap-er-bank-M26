f3b0ef6d3a18451d15fa73b6a3550d064c3b7d1c
noman-swe <noman.swe130@gmail.com>
2022-02-16 13:16:20 +0600

    26-8 (important) Money withdraw and reduce balance for withdraw

--- js/banking.js
+++ js/banking.js
@@ -13,15 +13,42 @@ document.getElementById('deposit-btn').addEventListener('click', function(){
     const newDepositTotal = previousDepositAmount + newDepositAmount;
     depositTotal.innerText = newDepositTotal; 
 
-
     // update account balance
-    const balanceTotal =  document.getElementById('balance-total');
-    const balanceTotalText = balanceTotal.innerText;
-    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const prevoiusBalanceTotal =  document.getElementById('balance-total');
+    const previousBalanceTotalText = prevoiusBalanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalText);
     const newBalanceTotal = previousBalanceTotal + newDepositAmount;
-    balanceTotal.innerText = newBalanceTotal;
-
+    prevoiusBalanceTotal.innerText = newBalanceTotal;
 
     //clear the deposit input
     depositInput.value = '';
+});
+
+// handle with withdraw handler 
+document.getElementById('withdraw-btn').addEventListener('click', function(){
+
+    // new amount from input field
+    const withdrawInput = document.getElementById('withdraw-input');
+    const newWithdrawAmountText = withdrawInput.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountText);
+
+    //exsisting amount
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotal.innerText = newWithdrawTotal;
+
+    // update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+    // set the value in balance section
+    balanceTotal.innerText = newBalanceTotal;    
+
+    // clear withdraw input value
+    withdrawInput.value = '';
 });
